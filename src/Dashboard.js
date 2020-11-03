8dcf35b246f3300dea15ed85165dc5d7aef359b6
devchea <rchea817@gmail.com>
2020-11-02 16:39:46 -0800

    "updates"

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,23 +1,25 @@
 import "./Dashboard.css";
+import data from './data.json'
 import React, { useState, useEffect } from "react";
-import firebase from "./firebase";
 import CardColumns from "react-bootstrap/CardColumns";
 import Container from "react-bootstrap/Container";
 import StockNameCard from './StockNameCard'
-import ProgressBar from "react-bootstrap/ProgressBar";
+
 
 
 const Dashboard = () => {
-  const [stocks, setStocks] = useState([null]);
 
+  const url = data
+  const [stocks, setStocks] = useState([null]);
+  
   useEffect(() => {
     const fetchData = async () => {
-      const db = firebase.firestore();
-      const data = await db.collection("stocks").get();
-      setStocks(data.docs.map((doc) => ({...doc.data()})));
-    };
-    fetchData();
-  }, []);
+      fetch(url)
+      .then(resp => console.log(resp))
+    }
+    fetchData()
+  }, [])
+
 
   return (
     <div>
@@ -25,7 +27,6 @@ const Dashboard = () => {
       ) ? (
         <h2>
           Loading...
-          <ProgressBar animated now={80} />
         </h2>
       ) : (
         <Container>
